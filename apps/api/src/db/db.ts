4ad57bec66ed812309c23689809b64bf96b82f68
HJK <danyil.butov@gmail.com>
2025-12-27 14:08:50 +0000

    fix: fix types

--- apps/api/src/db/db.ts
+++ apps/api/src/db/db.ts
@@ -1,9 +1,8 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
-import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
-export type SqlContext = PostgresJsDatabase<typeof schema>;
+export type SqlContext = NodePgDatabase<typeof schema>;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
