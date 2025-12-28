da67f42c85fdacef4483ae45fe5ef68ef325dca1
HJK <danyil.butov@gmail.com>
2025-12-28 20:23:25 +0000

    fix: vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,22 +1,9 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 import { nitro } from 'nitro/vite'
+import viteReact from '@vitejs/plugin-react'
 
-const config = defineConfig({
-  plugins: [
-    devtools(),
-    nitro(),
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+export default defineConfig({
+  plugins: [tanstackStart(), nitro(), viteReact()],
+  nitro: {},
 })
-
-export default config
