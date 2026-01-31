5b9a7be8c3d07fa4089dce641740999afc63aa95
HJK <danyil.butov@gmail.com>
2026-01-31 16:48:36 +0000

    fix: add default height

--- apps/web/src/components/layout/sheet.tsx
+++ apps/web/src/components/layout/sheet.tsx
@@ -12,7 +12,7 @@ export function EmptySheet({ children }: { children: ReactNode }) {
 
 function Sheet({ children }: { children: ReactNode }) {
   return (
-    <div className="relative z-10 -mt-px bg-surface flex flex-col mx-2 sm:mx-3 md:mx-0 md:w-3/4 lg:w-1/2 border-neutral-800 border rounded-md mb-5 w-[calc(100%-1rem)] sm:w-[calc(100%-1.5rem)]">
+    <div className="relative z-10 -mt-px bg-surface flex flex-col mx-2 sm:mx-3 md:mx-0 md:w-3/4 lg:w-1/2 border-neutral-800 border rounded-md mb-5 w-[calc(100%-1rem)] sm:w-[calc(100%-1.5rem)] min-h-[75vh]">
       <div className="p-4 sm:p-6 md:p-8 h-full">{children}</div>
     </div>
   )
